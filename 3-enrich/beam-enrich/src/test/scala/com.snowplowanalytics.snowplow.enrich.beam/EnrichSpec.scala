GlennJLea/snowplow

Beam Enrich: add metrics (closes #3737)

3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/EnrichSpec.scala
@@ -16,6 +16,7 @@ package com.snowplowanalytics.snowplow.enrich.beam
 
 import java.nio.file.{Path, Paths}
 
+import com.spotify.scio.ScioMetrics
 import com.spotify.scio.testing._
 import org.apache.commons.codec.binary.Base64
 
@@ -90,6 +91,22 @@ class EnrichSpec extends PipelineSpec {
         c.size == 1 && expected.forall(c.head.contains)
       })
       .output(PubsubIO[String]("bad"))(_ should beEmpty)
+      .distribution(Enrich.enrichedEventSizeDistribution) { d =>
+        d.count() shouldBe 1
+        d.min() shouldBe 818
+        d.min() shouldBe d.max()
+        d.min() shouldBe d.sum()
+        d.min() shouldBe d.mean()
+      }
+      .distribution(Enrich.timeToEnrichDistribution) { d =>
+        d.count() shouldBe 1
+        d.min() should be >= 100L
+        d.min() shouldBe d.max()
+        d.min() shouldBe d.sum()
+        d.min() shouldBe d.mean()
+      }
+      .counter(ScioMetrics.counter("snowplow", "vendor_com_google_analytics"))(_ shouldBe 1)
+      .counter(ScioMetrics.counter("snowplow", "tracker_js_0_13_1"))(_ shouldBe 1)
       .run()
   }
 

3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/EnrichWithLocalFileSpec.scala
@@ -20,6 +20,7 @@ import java.nio.file.Paths
 
 import scala.sys.process._
 
+import com.spotify.scio.ScioMetrics
 import com.spotify.scio.testing._
 import org.apache.commons.codec.binary.Base64
 
@@ -67,6 +68,22 @@ class EnrichWithLocalFileSpec extends PipelineSpec {
         c.size == 1 && expected.forall(c.head.contains)
       })
       .output(PubsubIO[String]("bad"))(_ should beEmpty)
+      .distribution(Enrich.enrichedEventSizeDistribution) { d =>
+        d.count() shouldBe 1
+        d.min() shouldBe 680
+        d.min() shouldBe d.max()
+        d.min() shouldBe d.sum()
+        d.min() shouldBe d.mean()
+      }
+      .distribution(Enrich.timeToEnrichDistribution) { d =>
+        d.count() shouldBe 1
+        d.min() should be >= 100L
+        d.min() shouldBe d.max()
+        d.min() shouldBe d.sum()
+        d.min() shouldBe d.mean()
+      }
+      .counter(ScioMetrics.counter("snowplow", "vendor_com_snowplowanalytics_snowplow"))(_ shouldBe 1)
+      .counter(ScioMetrics.counter("snowplow", "tracker_tracker_version"))(_ shouldBe 1)
       .run()
 
     deleteLocalFile("./ip_geo")

santhoshkumarvs/travis

3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/EnrichSpec.scala
@@ -16,6 +16,7 @@ package com.snowplowanalytics.snowplow.enrich.beam
 
 import java.nio.file.{Path, Paths}
 
+import com.spotify.scio.ScioMetrics
 import com.spotify.scio.testing._
 import org.apache.commons.codec.binary.Base64
 
@@ -90,6 +91,22 @@ class EnrichSpec extends PipelineSpec {
         c.size == 1 && expected.forall(c.head.contains)
       })
       .output(PubsubIO[String]("bad"))(_ should beEmpty)
+      .distribution(Enrich.enrichedEventSizeDistribution) { d =>
+        d.getCount() shouldBe 1
+        d.getMin() shouldBe 818
+        d.getMin() shouldBe d.getMax()
+        d.getMin() shouldBe d.getSum()
+        d.getMin() shouldBe d.getMean()
+      }
+      .distribution(Enrich.timeToEnrichDistribution) { d =>
+        d.getCount() shouldBe 1
+        d.getMin() should be >= 100L
+        d.getMin() shouldBe d.getMax()
+        d.getMin() shouldBe d.getSum()
+        d.getMin() shouldBe d.getMean()
+      }
+      .counter(ScioMetrics.counter("snowplow", "vendor_com_google_analytics"))(_ shouldBe 1)
+      .counter(ScioMetrics.counter("snowplow", "tracker_js_0_13_1"))(_ shouldBe 1)
       .run()
   }
 

3-enrich/beam-enrich/src/test/scala/com.snowplowanalytics.snowplow.enrich.beam/EnrichWithLocalFileSpec.scala
@@ -20,6 +20,7 @@ import java.nio.file.Paths
 
 import scala.sys.process._
 
+import com.spotify.scio.ScioMetrics
 import com.spotify.scio.testing._
 import org.apache.commons.codec.binary.Base64
 
@@ -67,6 +68,22 @@ class EnrichWithLocalFileSpec extends PipelineSpec {
         c.size == 1 && expected.forall(c.head.contains)
       })
       .output(PubsubIO[String]("bad"))(_ should beEmpty)
+      .distribution(Enrich.enrichedEventSizeDistribution) { d =>
+        d.getCount() shouldBe 1
+        d.getMin() shouldBe 680
+        d.getMin() shouldBe d.getMax()
+        d.getMin() shouldBe d.getSum()
+        d.getMin() shouldBe d.getMean()
+      }
+      .distribution(Enrich.timeToEnrichDistribution) { d =>
+        d.getCount() shouldBe 1
+        d.getMin() should be >= 100L
+        d.getMin() shouldBe d.getMax()
+        d.getMin() shouldBe d.getSum()
+        d.getMin() shouldBe d.getMean()
+      }
+      .counter(ScioMetrics.counter("snowplow", "vendor_com_snowplowanalytics_snowplow"))(_ shouldBe 1)
+      .counter(ScioMetrics.counter("snowplow", "tracker_tracker_version"))(_ shouldBe 1)
       .run()
 
     deleteLocalFile("./ip_geo")